f4618ce875ff43361f631717af1b4d5f998c9e63
Kevin Downs <kevin.downs04@gmail.com>
2019-11-05 04:26:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,13 @@ def countdown(num)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  countdown = num
+  while countdown > 0 
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1 
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
